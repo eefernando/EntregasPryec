ed19ca7584216e7c1967f46836cf0fad9880fd43
eefernando <144483862+eefernando@users.noreply.github.com>
2023-09-08 21:32:10 -0500

    Desarrollo ejercicio 17

---
+++ Ejercicio17.py
@@ -0,0 +1,11 @@
+## A = π r²
+## p ** 2 * r
+
+pi = 3.14
+
+radio = int(input('dime el radio de la circurferencia: '))
+
+area = pi * (radio**2)
+logitud = (2 * pi) * radio
+
+print(f"el area de la circurferencia es {area} y la longitud es {round(logitud, 2)}")
